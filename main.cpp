08ef7f00f87b56f8c416a34c3c8d55c4e862c63b
DennisChen11214 <87625335+DennisChen11214@users.noreply.github.com>
2021-07-22 16:10:14 -0400

    got rid of unnecessary includes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <ctype.h> 
-#include <sys/shm.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <pthread.h>
 #include <math.h>
 #include <vector>
 #include <iostream>
